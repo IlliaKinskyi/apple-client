0c25ac9d3776b4395d34080bb3ff29fe29783adb
x417v <ilyakinskiy@gmail.com>
2023-12-24 17:34:21 +0200

    Implement Sort items in catalog

--- components/modules/CatalogPage/FilterSelect.tsx
+++ components/modules/CatalogPage/FilterSelect.tsx
@@ -1,3 +1,9 @@
+import {
+  $items,
+  setItemsByPopularity,
+  setItemsCheapFirst,
+  setItemsExpensiveFirst,
+} from '@/context/items'
 import { $mode } from '@/context/mode'
 import {
   controlStyles,
@@ -5,25 +11,81 @@ import {
   selectStyles,
 } from '@/styles/catalog/select'
 import { optionStyles } from '@/styles/searchInput'
-import { SelectOptionType } from '@/types/common'
+import { IOption, SelectOptionType } from '@/types/common'
 import { createSelectOption } from '@/utils/common'
 import { categoriesOptions } from '@/utils/selectContents'
 import { useStore } from 'effector-react'
-import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import Select from 'react-select'
 
 const FilterSelect = () => {
   const mode = useStore($mode)
+  const items = useStore($items)
   const [categoryOption, setCategoryOption] = useState<SelectOptionType>(null)
+  const router = useRouter()
 
-  const handleSearchOptionChange = (selectedOption: SelectOptionType) => {
+  useEffect(() => {
+    if (items.rows) {
+      switch (router.query.first) {
+        case 'cheap':
+          updateCategoryOption('$ -> $$$')
+          setItemsCheapFirst()
+          break
+        case 'expensive':
+          updateCategoryOption('$$$ -> $')
+          setItemsExpensiveFirst()
+          break
+        case 'popular':
+          setItemsByPopularity()
+          updateCategoryOption('Popularity')
+          break
+        default:
+          updateCategoryOption('$ -> $$$')
+          setItemsCheapFirst()
+          break
+      }
+    }
+  }, [items.rows, router.query.first])
+
+  const updateCategoryOption = (value: string) =>
+    setCategoryOption({ value, label: value })
+
+  const updateRoteParam = (first: string) =>
+    router.push(
+      {
+        query: {
+          ...router.query,
+          first,
+        },
+      },
+      undefined,
+      { shallow: true }
+    )
+
+  const handleSortOptionChange = (selectedOption: SelectOptionType) => {
     setCategoryOption(selectedOption)
+
+    switch ((selectedOption as IOption).value) {
+      case '$ -> $$$':
+        setItemsCheapFirst()
+        updateRoteParam('cheap')
+        break
+      case '$$$ -> $':
+        setItemsExpensiveFirst()
+        updateRoteParam('expensive')
+        break
+      case 'Popularity':
+        setItemsByPopularity()
+        updateRoteParam('popular')
+        break
+    }
   }
   return (
     <Select
       placeholder="I'm looking..."
       value={categoryOption || createSelectOption('$ -> $$$')}
-      onChange={handleSearchOptionChange}
+      onChange={handleSortOptionChange}
       styles={{
         ...selectStyles,
         control: (defaultStyles) => ({
--- context/items.ts
+++ context/items.ts
@@ -5,6 +5,22 @@ const items = createDomain()
 
 export const setItems = items.createEvent<IItems>()
 
+export const setItemsCheapFirst = items.createEvent()
+export const setItemsExpensiveFirst = items.createEvent()
+export const setItemsByPopularity = items.createEvent()
+
 export const $items = items
   .createStore<IItems>({} as IItems)
   .on(setItems, (_, products) => products)
+  .on(setItemsCheapFirst, (state) => ({
+    ...state,
+    rows: state.rows.sort((a, b) => a.price - b.price),
+  }))
+  .on(setItemsExpensiveFirst, (state) => ({
+    ...state,
+    rows: state.rows.sort((a, b) => b.price - a.price),
+  }))
+  .on(setItemsByPopularity, (state) => ({
+    ...state,
+    rows: state.rows.sort((a, b) => b.popularity - a.popularity),
+  }))
