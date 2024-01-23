7f4b2a935146bcbee49afa8ed39b4bec23e6d19b
x417v <ilyakinskiy@gmail.com>
2024-01-23 14:49:05 +0200

    Implemented location detection

---
+++ app/api/geolocation.ts
@@ -0,0 +1,14 @@
+import { IGeolocation } from '@/types/common'
+import { createEffect } from 'effector-next'
+import api from '../axiosClient'
+
+export const getGeolocationFx = createEffect(
+  async ({ latitude, longitude }: IGeolocation) => {
+    const data = await api.get(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${process.env.NEXT_PUBLIC_GEOPI_KEY}`,
+      { withCredentials: false }
+    )
+
+    return data
+  }
+)
